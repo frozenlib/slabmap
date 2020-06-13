2eb278907dea5418cde72c9cde494aa3fea75a76
frozenlib <frozenlib@users.noreply.github.com>
2020-06-13 18:35:44 +0900

    Remove unnecessary block.

--- src/lib.rs
+++ src/lib.rs
@@ -82,18 +82,16 @@ impl<T> Slab<T> {
                 return Some(value);
             }
             replace(&mut self.entries[index], e);
-        } else {
-            if index + 1 == self.entries.len() {
-                let e = self.entries.remove(index);
-                if let Entry::Occupied(value) = e {
-                    self.len -= 1;
-                    if self.len == 0 {
-                        self.clear();
-                    }
-                    return Some(value);
+        } else if index + 1 == self.entries.len() {
+            let e = self.entries.remove(index);
+            if let Entry::Occupied(value) = e {
+                self.len -= 1;
+                if self.len == 0 {
+                    self.clear();
                 }
-                self.entries.push(e);
+                return Some(value);
             }
+            self.entries.push(e);
         }
         None
     }
